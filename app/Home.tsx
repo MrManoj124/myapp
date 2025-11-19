ea426717312990fdbc9b08de547801388bea99fd
MrManoj124 <manojmanorooban@gmail.com>
2025-11-20 01:10:47 +0530

    Create a function inside the Home.tsx

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,2 +1,15 @@
 import {StyleSheet, Text, View} from "react-native";
 import React from "react";
+
+export default function Home()
+{
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}> WelCome</Text>
+            <Text style={styles.subtitle}> Open for more details </Text>
+
+        </View>
+    )
+
+}
