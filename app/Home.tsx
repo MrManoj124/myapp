31337e6a727c222b0661f9bdd3a244b73ddbd752
MrManoj124 <manojmanorooban@gmail.com>
2025-11-20 01:17:45 +0530

    Added styles to Home.tsx file

--- app/Home.tsx
+++ app/Home.tsx
@@ -13,3 +13,32 @@ export default function Home()
     )
 
 }
+
+const styles=StyleSheet.create(
+{
+    container:{
+        flex: 1,
+        backgroundColor: "#F5F7FF",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 25,
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "800",
+        marginTop: 20,
+        color: "#111",
+        textAlign: "center",
+        letterSpacing: 0.5,
+      },
+      subtitle: {
+        fontSize: 17,
+        color: "#6a6a6a",
+        textAlign: "center",
+        paddingHorizontal: 25,
+        marginTop: 10,
+        lineHeight: 22,
+      },
+    
+    
+})
